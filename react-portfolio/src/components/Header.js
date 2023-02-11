fc550d1601dbe76e30c03c864ebbb2dc2dfba5c9
Olasunkanmi <olasunkanmi.m.owolabi@gmail.com>
2023-02-11 19:53:30 +0000

    updated main and css

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <h1>Olasunkanmi</h1>
+            <nav>
+                <a href="#About">About</a>
+                <a href="#Work">Work</a>
+                <a href="#Contact">Contact</a>
+            </nav>
+        </header>
+    )
+}
+export default Header;
---
+++ react-portfolio/src/components/Main.js
@@ -0,0 +1,150 @@
+
+import picture from './'
+
+
+function Main() {
+    return (
+        <main>
+  <div className="hero-banner">
+    <h2>Olasunkanmi Moses Owolabi</h2>
+    <h3>Front-End Web Developer</h3>
+    <h4>HTML | CSS | JAVASCRIPT | REACT</h4>
+  </div>
+  <h3 className="category" id="About">
+    About
+  </h3>
+  <span className="about">
+    <img src={picture} alt=" "/>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+      repudiandae fuga eaque, assumenda dolores a necessitatibus laudantium
+      temporibus repellat ducimus, sunt omnis non, modi earum sapiente aperiam.
+      Minus, debitis modi! Dolores minus earum aperiam consequuntur quam soluta
+      tenetur molestiae accusamus ipsum, excepturi doloribus voluptas? Nesciunt
+      reiciendis, earum aliquid quasi voluptatibus at quis perferendis
+      reprehenderit, iste suscipit nisi quas voluptatem ipsa!
+    </p>
+  </span>
+  <h3 className="category" id="Work">
+    Work
+  </h3>
+  <div className="projects item1">
+    <img src="./Assets/Images/weather-dashboard.jpg" alt="" />
+    <span>
+      <h3>weather-dashboard</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi,
+        nemo harum nobis quae voluptatum vel reiciendis rem aliquam. At,
+        reprehenderit repudiandae dolore ratione similique ipsam veritatis
+        doloribus culpa tenetur?
+      </p>
+      <a href="https://olasunkanmi-o.github.io/weather-dashboard/">
+        Link to the Project
+      </a>
+    </span>
+  </div>
+  <div className="projects item2">
+    <span>
+      <h3>Quizz Challenge</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi,
+        nemo harum nobis quae voluptatum vel reiciendis rem aliquam. At,
+        reprehenderit repudiandae dolore ratione similique ipsam veritatis
+        doloribus culpa tenetur?
+      </p>
+      <a href="https://olasunkanmi-o.github.io/Timed-Quiz/">
+        Link to the Project
+      </a>
+    </span>
+    <img src="./Assets/Images/Timed Quizz.jpg" alt="" />
+  </div>
+  <div className="projects item3">
+    <img src="./Assets/Images/Password Generator.jpg" alt="" />
+    <span>
+      <h3>Password Generator</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi,
+        nemo harum nobis quae voluptatum vel reiciendis rem aliquam. At,
+        reprehenderit repudiandae dolore ratione similique ipsam veritatis
+        doloribus culpa tenetur?
+      </p>
+      <a href="https://olasunkanmi-o.github.io/Password_Generator/">
+        Link to the Project
+      </a>
+    </span>
+  </div>
+  <div className="projects item4">
+    <span>
+      <h3>README Generator</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi,
+        nemo harum nobis quae voluptatum vel reiciendis rem aliquam. At,
+        reprehenderit repudiandae dolore ratione similique ipsam veritatis
+        doloribus culpa tenetur?
+      </p>
+      <a href="https://olasunkanmi-o.github.io/ReadMe-Generator/">
+        Link to the Project
+      </a>
+    </span>
+    <img src="./Assets/Images/README generator.jpg" alt="" />
+  </div>
+  <div className="projects item5">
+    <img src="./Assets/Images/Fetchbook.jpg" alt="" />
+    <span>
+      <h3>Fetchbook</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi,
+        nemo harum nobis quae voluptatum vel reiciendis rem aliquam. At,
+        reprehenderit repudiandae dolore ratione similique ipsam veritatis
+        doloribus culpa tenetur?
+      </p>
+      <a href="https://c-navarroperez.github.io/fetchbook/">
+        Link to the Project
+      </a>
+    </span>
+  </div>
+  <div className="projects item6">
+    <span>
+      <h3>Title</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi,
+        nemo harum nobis quae voluptatum vel reiciendis rem aliquam. At,
+        reprehenderit repudiandae dolore ratione similique ipsam veritatis
+        doloribus culpa tenetur?
+      </p>
+      <a href="https://github.com/Olasunkanmi-o">Link to the Project</a>
+    </span>
+    <img src="./Assets/Images/02-portfolio-4.jpg" alt="" />
+  </div>
+  <h3 className="category" id="Contact">
+    Contact
+  </h3>
+  <div className="formitem">
+    <form>
+      <label htmlFor="name">Your Name:</label>
+      <input type="text" />
+      <label htmlFor="email">Your Email:</label>
+      <input type="text" />
+      <label htmlFor="message">Send me Message:</label>
+      <textarea
+        name="message"
+        id="message"
+        cols={30}
+        rows={10}
+        defaultValue={""}
+      />
+      <input type="Submit" />
+    </form>
+    <div className="contact">
+      <a href="https://github.com/Olasunkanmi-O">Github</a>
+      <a href="https://www.linkedin.com/in/olasunkanmi-owolabi/">Linkedin</a>
+      <a href="mailto:olasunkanmi.m.owolabi@gmail.com">Email</a>
+    </div>
+  </div>
+</main>
+
+    )
+}
+
+export default Main;
+
